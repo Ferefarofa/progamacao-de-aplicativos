14f4a8e89c46b47e9b33ce673f4988379979eb63
Felipe Reche <felipe.reche01107178@sesisenaipr.org.br>
2026-02-20 11:54:26 -0200

    Aula dia 20/02

---
+++ aluno.py
@@ -0,0 +1,9 @@
+nome = (input("Insira o seu nome: "))
+idade = int(input("insira sua idade: "))
+telefone = int(input("insira seu telefone: "))
+cidade = (input("insira sua cidade: "))
+
+print("Olá", nome)
+print("Sua idade é: ", idade)
+print("Seu telefone é: ", telefone)
+print("Voce mora em: ", cidade)
---
+++ matematica.py
@@ -0,0 +1,12 @@
+valor1 = int(input("digite um valor"))
+valor2 = int(input("digite um valor"))
+
+soma = valor1 + valor2
+subtracao = valor1 - valor2
+multiplicacao = valor1 * valor2
+divisao = valor1 / valor2
+
+print("O resultado da soma deve ser: ", soma)
+print("O resultado da subtracao deve ser: ", subtracao)
+print("O resultado da divisao deve ser: ", divisao)
+print("O resultado da multiplicacao deve ser", multiplicacao)
