d2a68b9c6dcc1f3edcdd94c171aaee0212bd3648
Felipe Reche <felipe.reche01107178@sesisenaipr.org.br>
2026-03-12 10:03:39 -0300

    atividades 12/03

---
+++ seldados.py
@@ -0,0 +1,2 @@
+filmes = ["Demon slayer-Infinity Castle", "Minecraft", "Capitã Marvel", "Vingadores-Guerra infinita", "Wall-E"]
+print("Imprimindo filmes: ", (filmes[0]),",", (filmes[4]))
---
+++ testelista.py
@@ -0,0 +1,2 @@
+frutas = ["banana", "manga", "abacaxi"]
+print(frutas[1])
