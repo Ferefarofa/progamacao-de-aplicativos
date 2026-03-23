47c2ee7c8317ee48cebffc986b971d682d38c892
Felipe Reche <felipe.reche01107178@sesisenaipr.org.br>
2026-03-23 12:44:38 -0300

    atividades 23/03

--- sistemacontrole.py
+++ sistemacontrole.py
@@ -1,4 +1,13 @@
 autorizados =  ["Alice", "Bob", "Carlos"]
-nome = input("Digite o nome do pesquisador: ")
-if nome == True:
-    print("")
+nome = input("\nDigite o nome do pesquisador: ")
+if nome == "Alice" or "Bob" or "Carlos":
+    print(f"\nAcesso Permitido! O pesquisador {nome} está na posição",autorizados.index(nome)) 
+    pergunta = input("\nVocê quer remover o nome da lista ?(Sim/Não): ")
+if pergunta == "Sim":
+    autorizados.remove(nome)
+    print(f"\nAgora os membros da lista são {autorizados}")
+else:
+    print(f"\nAcesso Negado! O pesquisador {nome} não foi encontrado.")
+    perg2 = input("\nQuer cadastrar esse nome como novo pesquisador ?(Sim/Não): ")
+    if perg2 == "Sim":
+        autorizados.append(nome)
