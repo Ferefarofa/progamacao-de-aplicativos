e9c3c615614f562d998a74c41ee3ab43639f3077
Felipe Reche <felipe.reche01107178@sesisenaipr.org.br>
2026-03-26 10:02:55 -0300

    atividades 26/03

--- entradadados.py
+++ entradadados.py
@@ -1,6 +1,7 @@
 compras = []
-while compras != "fim":
+item = ""
+while item != "fim":
     item = input("Item a ser adicionado na lista: ")
     compras.append(item)
-if compras == "fim":
-    print("Inserção de itens finalizada, lista final: ", compras)
+compras.remove("fim")
+print(f"Fim da lista de compras, a lista atual agora é: {compras}")
