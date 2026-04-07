7851249f4683338214bf773eb215f2a307c760c2
Felipe Reche <felipe.reche01107178@sesisenaipr.org.br>
2026-04-07 09:47:44 -0300

    atividades 07/04

--- localizadorpecas.py
+++ localizadorpecas.py
@@ -1,8 +1,8 @@
 ferramentas = ["Chave Inglesa", "Alicate", "Martelo", "Parafusadeira"]
 perg = input("Qual ferramenta você procura ?: ")
 for ferramenta in ferramentas:
-        if perg == ferramenta:
-            print(f"\nFerramenta {ferramenta} encontrada no índice {ferramentas.index(ferramenta)}")
+    if perg == ferramenta:
+        print(f"\nFerramenta {ferramenta} encontrada no índice {ferramentas.index(ferramenta)}")
 while perg in ferramentas:
     perg = input("Qual ferramenta você procura ?: ")
     for ferramenta in ferramentas:
@@ -11,9 +11,12 @@ while perg in ferramentas:
 else:
     pregunta = input("Ferramenta não encontrada, deseja adiciona-la a caixa de ferramentas ?(S/N): ")
     if pregunta == "S":
-        ferramentas.append(perg)
-        pergunta =""
-    while pergunta != "N":
+        ferramentas.append(perg) 
+        while pregunta != "N":
             pregunta = input("Ferramenta adicionada! Deseja adicionar mais alguma?(S/N) : ")
-            if pergunta == "S":
+            if pregunta == "S":
                 outra = input("Qual ferramenta?: ")
+                ferramentas.append(outra)
+    if pregunta == "N":
+        print(f"ferramentas disponiveis: {ferramentas}")
+
