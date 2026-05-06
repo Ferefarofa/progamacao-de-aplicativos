b5089c75310f8696d325dbf1a9afd6a83675536e
Felipe Reche <felipe.reche01107178@sesisenaipr.org.br>
2026-05-06 11:56:06 -0300

    atividades 06/05

--- sistemacrudmodularizado.py
+++ sistemacrudmodularizado.py
@@ -1,25 +1,26 @@
 estoque = []
 acao = 0
-def adicionar_produto(nome):
+
+def adicionar_produto(nome): #É uma função que não recebe valores válidos em seus parâmetros, seu valor se torna útil quando o usuário o altera no input dentro da função. É a função responsavel por adicionar itens na lista(estoque).
     nome = input("Digite o nome do produto: ")
     estoque.append(nome)
 
-def listar_produtos(estoque):
+def listar_produtos(estoque): #É uma função que recebe predefinidamente a lista "estoque" em seus parâmetros, lista os itens presentes dentro da lista e seus respectivos índices.
     for item in estoque:
-        print(f"{estoque.index(item)} - {item}")
+        print(f"\n{estoque.index(item)} - {item}")
 
-def atualizar_produto(indice, produto):
+def atualizar_produto(indice, produto): #É uma função que não recebe valores válidos em seus parâmetros[...]. É uma função responsavel por atualizar valores dentro da lista.
     indice = int(input("Qual o indice do produto ?: "))
     produto = input("Qual o nome do produto ?: ")
     estoque[indice] = produto
 
-def remover_produto(indice):
+def remover_produto(indice): #É uma função que não recebe valores válidos em seus parâmetros[...]. É uma função responsavel por remover algum produto da lista pelo índice.
     indice = int(input("Qual o indice do produto a ser removido ?: "))
     removido = estoque[indice]
     estoque.pop(indice)
     print(f"Item {removido} removido. ")
 
-def exibir_menu(acao):
+def exibir_menu(acao): #Função coração do programa, executa todas as outras funções anteriores e coloca o programa em funcionamento.
     while acao != 5:
         print("\n1 - Adicionar produto")
         print("\n2 - Listar produtos ")
