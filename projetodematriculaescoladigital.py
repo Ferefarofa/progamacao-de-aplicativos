4185bf774a15ce429ef7c5b152922186633e6fc6
Felipe Reche <felipe.reche01107178@sesisenaipr.org.br>
2026-05-21 10:07:26 -0300

    atividades 21/05

--- projetodematriculaescoladigital.py
+++ projetodematriculaescoladigital.py
@@ -3,32 +3,54 @@ import json
 def criar_arquivo():
     open("matriculas.json", 'w').close()
     with open("matriculas.json", 'w') as matriculas:
-        dados = {"CPF": "Sem dados",
-                "nome": "Sem dados",
-                "telefone": "Sem dados",
-                "turma": "Sem dados",
-                "idade": "Sem dados"
+        dados = {"cpf": ["Sem dados"],
+                "nome": ["Sem dados"],
+                "telefone": ["Sem dados"],
+                "turma": ["Sem dados"],
+                "idade": ["Sem dados"]
                 }
         json.dump(dados, matriculas, indent=4, ensure_ascii=False)
         
 def cadastrar_aluno():
-    cpf = int(input("Digite o CPF do aluno: "))
-    nome = input("Digite o nome do aluno: ")
-    telefone = int(input("Digite o telefone do aluno: "))
-    turma = input("Digite a turma do aluno: ")
-    idade = int(input("Digite a idade do aluno: "))
     with open("matriculas.json", 'r') as matriculas:
         dados = json.load(matriculas)
-        if dados["CPF"] == cpf:
-            print("CPF já existe ")
-        else:
-            dados["CPF"] = cpf
-            dados["nome"] = nome
-            dados["telefone"] = telefone
-            dados["turma"] = turma
-            dados["idade"] = idade
+    cpf = input("Digite o CPF do aluno: ")
+    if cpf in dados["cpf"]:
+        print("CPF já existe ")
+    else:
+        nome = input("Digite o nome do aluno: ")
+        telefone = input("Digite o telefone do aluno: ")
+        turma = input("Digite a turma do aluno: ")
+        idade = input("Digite a idade do aluno: ")
+        dados["cpf"].append(cpf)
+        dados["nome"].append(nome)
+        dados["telefone"].append(telefone)
+        dados["turma"].append(turma)
+        dados["idade"].append(idade)
+        with open("matriculas.json", 'w') as matriculas:
+            json.dump(dados, matriculas, indent=4, ensure_ascii=False)
 
 def listar():
+    quem = input("Digite o cpf que deseja verificar: ")
     with open("matriculas.json", 'r') as matriculas:
         dados = json.load(matriculas)
-    print(f"{dados}")
+        idx = dados["cpf"].index(quem)
+    print(f"{dados["cpf"][idx], dados["nome"][idx], dados["telefone"][idx], dados["turma"][idx], dados["idade"][idx]}")
+
+def atualizar():
+    quem = input("Insira o cpf a ser alterado: ")
+    mudar = input("Insira o campo a ser atualizado: ")
+    mudanca = input("Insira o novo dado: ")
+    if mudar == "cpf":
+        print("Não é possível mudar o cpf de cadastro. ")
+    else:
+        with open("matriculas.json", 'r') as matriculas:
+            dados = json.load(matriculas)
+            dados[quem][mudar] = mudanca
+
+        with open("matriculas.json", 'w') as matriculas:
+            json.dump(dados, matriculas, indent=4, ensure_ascii=False)
+
+def remover():
+    quem = input("Digite o cpf a ser removido")
+
