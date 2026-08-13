20cc4b953cce51fbe26ff2ba1e582cb4902afbfa
Felipe Reche <felipe.reche01107178@sesisenaipr.org.br>
2026-08-13 09:42:56 -0300

    prova do 2 trimestre 13/08

---
+++ prova2trimestrepasta/prova2trimestre.py
@@ -0,0 +1,246 @@
+import sqlite3
+conexao = sqlite3.connect('prova2trimestrepasta/banco_mercado.db')
+cursor = conexao.cursor()
+cursor.execute("PRAGMA foreign_keys = ON")
+ 
+def cadastrar_rede():
+    conexao = sqlite3.connect('prova2trimestrepasta/banco_mercado.db')
+    cursor = conexao.cursor()
+    cursor.execute("PRAGMA foreign_keys = ON")
+
+
+    nome = input("Digite o nome da rede: ")
+    cnpj = int(input("Digite o CNPJ da rede: "))
+    
+    cursor.execute('''
+                CREATE TABLE IF NOT EXISTS redes(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                cnpj INTEGER NOT NULL
+                )''')
+
+
+    comando_inserir = f'''
+        INSERT INTO redes(nome, cnpj)
+        VALUES('{nome}', {cnpj})'''
+
+    cursor.execute(comando_inserir)
+    conexao.commit()
+    conexao.close()
+
+
+
+
+
+def cadastrar_loja():
+    conexao = sqlite3.connect('prova2trimestrepasta/banco_mercado.db')
+    cursor = conexao.cursor()
+    cursor.execute("PRAGMA foreign_keys = ON")
+
+
+    nome = input("Digite o nome da loja: ")
+    endereco = input("Digite o endereco da loja: ")
+    id_rede = int(input("Digite o id da rede vinculado a loja: "))
+    
+    cursor.execute('''
+                CREATE TABLE IF NOT EXISTS lojas(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                endereco TEXT NOT NULL,
+                id_rede INTEGER NOT NULL,
+                FOREIGN KEY (id_rede) REFERENCES redes(id)
+                )''')
+
+                
+    comando_inserir = f'''
+        INSERT INTO lojas(nome, endereco, id_rede)
+        VALUES('{nome}', '{endereco}', {id_rede})'''
+
+    cursor.execute(comando_inserir)
+    conexao.commit()
+
+    
+    conexao.close()
+    
+def listar_redes():
+    conexao.commit()
+    cursor.execute("SELECT * FROM redes")
+    redes = cursor.fetchall()
+    for rede in redes:
+        print(f"=======================\n ID: {rede[0]}\n nome: {rede[1]}\n cnpj: {rede[2]}\n=======================")
+
+def listar_lojas():
+    conexao.commit()
+    cursor.execute("SELECT * FROM lojas")
+    lojas = cursor.fetchall()
+    for loja in lojas:
+        print(f"=======================\n ID: {loja[0]}\n nome: {loja[1]}\n endereço: {loja[2]}\n ID/Rede vinculada: {loja[3]}\n=======================")
+
+
+def atualizar_rede():
+    conexao = sqlite3.connect('prova2trimestrepasta/banco_mercado.db')
+    cursor = conexao.cursor()
+    cursor.execute("PRAGMA foreign_keys = ON")
+
+    id_rede = int(input("Digite o id da rede: "))
+    novo_nome = input("Digite o novo nome da rede: ")
+    novo_cnpj = int(input("Digite o novo cnpj: "))
+    
+
+    cursor.execute(f'''
+                   UPDATE redes
+                    SET nome = '{novo_nome}', cnpj = {novo_cnpj} WHERE id = {id_rede}''')
+    conexao.commit()
+    print("Dados atualizados com sucesso! ")
+    conexao.close()
+
+def atualizar_loja():
+    conexao = sqlite3.connect('prova2trimestrepasta/banco_mercado.db')
+    cursor = conexao.cursor()
+    cursor.execute("PRAGMA foreign_keys = ON")
+
+    id_loja = int(input("Digite o id da loja: "))
+    novo_nome = input("Digite o novo nome da loja: ")
+    novo_endereco = input("Digite o novo endereco da loja: ")
+    novo_id_rede = int(input("Digite a nova rede vinculada: "))
+    
+
+    cursor.execute(f'''
+                   UPDATE lojas
+                    SET nome = '{novo_nome}', endereco = '{novo_endereco}', id_rede = {novo_id_rede}  WHERE id = {id_loja}''')
+    conexao.commit()
+    print("Dados atualizados com sucesso! ")
+    conexao.close()
+
+
+def remover_rede():
+    try:
+        conexao = sqlite3.connect('prova2trimestrepasta/banco_mercado.db')
+        cursor = conexao.cursor()
+        cursor.execute("PRAGMA foreign_keys = ON")
+
+
+        id_rede = int(input("Digite o ID da rede que deseja remover: "))
+        cursor.execute(
+            f"DELETE FROM redes WHERE id = {id_rede}"
+        )
+
+        conexao.commit()
+        print("Rede removida com sucesso")
+    except sqlite3.IntegrityError:
+        print("A rede deve ser livre de vinculos para ser excluida.")
+    finally:
+        conexao.close()
+
+
+def remover_loja():
+    conexao = sqlite3.connect('prova2trimestrepasta/banco_mercado.db')
+    cursor = conexao.cursor()
+    cursor.execute("PRAGMA foreign_keys = ON")
+
+    
+    id_loja = int(input("Digite o ID da loja que deseja remover: "))
+    cursor.execute(
+        f"DELETE FROM lojas WHERE id = {id_loja}"
+    )
+
+    conexao.commit()
+    print("Loja removida com sucesso")
+    conexao.close()
+
+def menu_redes():
+    try:
+        
+        opcao = ""
+        while opcao != 5:
+            print("==================================\n 1 - CADASTRAR REDE\n 2 - LISTAR REDES\n 3 - ATUALIZAR REDES\n 4 - REMOVER REDES\n 5 - SAIR\n==================================")
+            opcao = int(input("Digite sua ação: "))
+            if opcao == 1:
+                cadastrar_rede()
+            elif opcao == 2:
+                listar_redes()
+            elif opcao == 3:
+                atualizar_rede()
+            elif opcao == 4:
+                remover_rede()
+            elif opcao == 5:
+                break
+            elif opcao != 1 or 2 or 3 or 4 or 5:
+                print("Insira uma opção válida")
+    except ValueError:
+        print("Não digite letras em campos numéricos e vice-versa")
+    finally:
+        if opcao != 5:
+            print("Tente novamente")
+            menu_redes()
+        elif opcao == 5:
+            print("Fechando menu das redes")
+        else:
+            menu_redes()
+
+
+def menu_lojas():
+    try:
+        
+        opcao = ""
+        while opcao != 5:
+            print("==================================\n 1 - CADASTRAR LOJA\n 2 - LISTAR LOJAS\n 3 - ATUALIZAR LOJAS\n 4 - REMOVER LOJAS\n 5 - SAIR\n==================================")
+            opcao = int(input("Digite sua ação: "))
+            if opcao == 1:
+                cadastrar_loja()
+            elif opcao == 2:
+                listar_lojas()
+            elif opcao == 3:
+                atualizar_loja()
+            elif opcao == 4:
+                remover_loja()
+            elif opcao == 5:
+                break
+            elif opcao != 1 or 2 or 3 or 4 or 5:
+                print("Insira uma opção válida")
+    except ValueError:
+        print("Não digite letras em campos numéricos e vice-versa")
+    except sqlite3.IntegrityError:
+        print("ID de vínculo não existe")
+    finally:
+        if opcao != 5:
+            print("Tente novamente")
+            menu_lojas()
+        elif opcao == 5:
+            print("Fechando menu das lojas")
+        else:
+            menu_lojas()
+
+
+def menu_principal():
+    try:
+        acao = ""
+        while True:
+            print(" SISTEMA DE MERCADOS PARANAVAÍ")
+            print("==================================\n 1 - MENU DE REDES\n 2 - MENU DE LOJAS\n 3 - ENCERRAR SISTEMA\n==================================\n")
+            acao = int(input("Digite a opção do menu para acessar: "))
+            if acao == 1:
+                menu_redes()
+            elif acao == 2:
+                menu_lojas()
+            elif acao == 3:
+                break
+            elif acao != (1 or 2 or 3):
+                print("Insira uma opção válida")
+        print("Volte sempre!")
+        
+    except ValueError:
+        print("Não digite letras em campos numéricos e vice-versa")
+    finally:
+        if acao != 3:
+            print("Tente novamente")
+            menu_principal()
+        elif acao == 3:
+            print("Fechando menu principal")
+        else:
+            menu_principal()
+
+
+menu_principal()
+
+        
